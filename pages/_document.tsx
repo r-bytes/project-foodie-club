1dd29fc4ce3eefbaac3c0f0e07fed89fc409ad72
r-bytes <101632683+r-bytes@users.noreply.github.com>
2022-11-24 19:57:10 +0100

    feat: MUI

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,8 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+
       </Head>
       <body>
         <Main />
